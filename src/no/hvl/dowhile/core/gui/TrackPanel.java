1c551019ce338017297164aaadab4762b4d335d2
Jarand95 <jarand95@gmail.com>
2017-04-19 10:04:02 +0200

    Redesigned the track panel.

--- src/no/hvl/dowhile/core/gui/TrackPanel.java
+++ src/no/hvl/dowhile/core/gui/TrackPanel.java
@@ -18,16 +18,12 @@ import java.util.List;
 public class TrackPanel extends JPanel {
     private final OperationManager OPERATION_MANAGER;
     private final Window WINDOW;
-    private JLabel statusLabel;
     private JLabel operationStartedLabel;
     private JLabel currentImportLabel;
     private JLabel remainingFilesLabel;
     private GridBagConstraints constraints;
     private List<JRadioButton> radioButtons;
     private ButtonGroup radioButtonGroup;
-
-    private String currentFile;
-    private int fileQueue;
 
     public TrackPanel(final OperationManager OPERATION_MANAGER, final Window WINDOW) {
         this.OPERATION_MANAGER = OPERATION_MANAGER;
@@ -51,30 +47,15 @@ public class TrackPanel extends JPanel {
                         + "</body></html>",
                 WINDOW.TEXT_FONT_SIZE
         );
-        WINDOW.setConstraintsXY(constraints, 0, 1);
+        WINDOW.setConstraintsXY(constraints, 2, 0);
         constraints.gridwidth = 2;
         add(operationStartedLabel, constraints);
 
-
-
-
-        // isConnected label
-        statusLabel = WINDOW.makeLabel(Messages.GPS_OFFLINE.get(), WINDOW.TEXT_FONT_SIZE);
-        WINDOW.setConstraintsXY(constraints, 3, 1);
-        constraints.anchor = GridBagConstraints.NORTH;
-        add(statusLabel, constraints);
-
         // Current file imported from GPS
-        String currentImportedFile = Messages.IMPORTED_FROM_GPS.get() + currentFile;
+        String currentImportedFile = Messages.IMPORTED_FROM_GPS.get() + "Ingen fil.";
         currentImportLabel = WINDOW.makeLabel(currentImportedFile, WINDOW.TEXT_FONT_SIZE);
-        WINDOW.setConstraintsXY(constraints, 1, 1);
+        WINDOW.setConstraintsXY(constraints, 0, 1);
         add(currentImportLabel, constraints);
-
-        // Remaining files imported from GPS waiting to be processed
-        String remainingFiles = fileQueue + Messages.IMPORTED_FILES_LEFT_TO_PROSESS.get();
-        remainingFilesLabel = WINDOW.makeLabel(remainingFiles, WINDOW.TEXT_FONT_SIZE);
-        WINDOW.setConstraintsXY(constraints, 1, 2);
-        add(remainingFilesLabel, constraints);
 
         // adding buttons
         radioButtonGroup = new ButtonGroup();
@@ -83,51 +64,58 @@ public class TrackPanel extends JPanel {
 
         // Label and input for team number
         JLabel crewNumberLabel = WINDOW.makeLabel(Messages.CREW_NUMBER.get(), WINDOW.TEXT_FONT_SIZE);
-        WINDOW.setConstraintsXY(constraints, 2, 3);
+        WINDOW.setConstraintsXY(constraints, 1, 3);
+        constraints.gridwidth = 1;
         add(crewNumberLabel, constraints);
 
         SpinnerModel crewNumberInput = new SpinnerNumberModel(0, 0, 15, 1);
         JSpinner groupNumberSpinner = new JSpinner(crewNumberInput);
-        WINDOW.setConstraintsXY(constraints, 2, 4);
+        WINDOW.setConstraintsXY(constraints, 1, 4);
         constraints.fill = GridBagConstraints.HORIZONTAL;
         add(groupNumberSpinner, constraints);
 
         // Label and input for crew count
         JLabel crewCountLabel = WINDOW.makeLabel(Messages.CREW_COUNT.get(), WINDOW.TEXT_FONT_SIZE);
-        WINDOW.setConstraintsXY(constraints, 2, 5);
+        WINDOW.setConstraintsXY(constraints, 1, 5);
         add(crewCountLabel, constraints);
 
         SpinnerModel crewCountInput = new SpinnerNumberModel(0, 0, 15, 1);
         JSpinner crewCountSpinner = new JSpinner(crewCountInput);
-        WINDOW.setConstraintsXY(constraints, 2, 6);
+        WINDOW.setConstraintsXY(constraints, 1, 6);
         constraints.fill = GridBagConstraints.HORIZONTAL;
         add(crewCountSpinner, constraints);
 
         // Label and input for area searched
         JLabel areaLabel = WINDOW.makeLabel(Messages.AREA_SEARCHED.get(), WINDOW.TEXT_FONT_SIZE);
-        WINDOW.setConstraintsXY(constraints, 4, 3);
+        WINDOW.setConstraintsXY(constraints, 3, 3);
         add(areaLabel, constraints);
 
         JTextField areaInput = new JTextField();
-        WINDOW.setConstraintsXY(constraints, 4, 4);
+        WINDOW.setConstraintsXY(constraints, 3, 4);
         add(areaInput, constraints);
 
         // Label and input for track number
         JLabel trackNumberLabel = WINDOW.makeLabel(Messages.TRACK_NUMBER.get(), WINDOW.TEXT_FONT_SIZE);
-        WINDOW.setConstraintsXY(constraints, 4, 5);
+        WINDOW.setConstraintsXY(constraints, 3, 5);
         add(trackNumberLabel, constraints);
 
         SpinnerModel trackNumberInput = new SpinnerNumberModel(0, 0, 15, 1);
         JSpinner trackNumberSpinner = new JSpinner(trackNumberInput);
-        WINDOW.setConstraintsXY(constraints, 4, 6);
+        WINDOW.setConstraintsXY(constraints, 3, 6);
         constraints.fill = GridBagConstraints.HORIZONTAL;
         add(trackNumberSpinner, constraints);
 
         // Register button
         JButton registerButton = new JButton(Messages.REGISTER_BUTTON.get());
-        WINDOW.setConstraintsXY(constraints, 2, 7);
-        constraints.gridwidth = 2;
+        WINDOW.setConstraintsXY(constraints, 0, 9);
+        constraints.gridwidth = 4;
         add(registerButton, constraints);
+
+        // Remaining files imported from GPS waiting to be processed
+        String remainingFiles = Messages.IMPORTED_FILES_LEFT_TO_PROCESS.get("" + 0);
+        remainingFilesLabel = WINDOW.makeLabel(remainingFiles, WINDOW.TEXT_FONT_SIZE);
+        WINDOW.setConstraintsXY(constraints, 0, 10);
+        add(remainingFilesLabel, constraints);
 
         registerButton.addActionListener(new ActionListener() {
             @Override
@@ -150,28 +138,11 @@ public class TrackPanel extends JPanel {
         });
     }
 
-    public void setCurrentFile(String currentFile) {
-        this.currentFile = currentFile;
-    }
-
-    public void setFileQueue(int fileQueue) {
-        this.fileQueue = fileQueue;
-    }
-
     public void updateCurrentFile(String filename, int filesLeft) {
         String currentImportedFile = Messages.IMPORTED_FROM_GPS.get() + filename;
-        String remainingFiles = filesLeft + Messages.IMPORTED_FILES_LEFT_TO_PROSESS.get();
+        String remainingFiles = Messages.IMPORTED_FILES_LEFT_TO_PROCESS.get("" + filesLeft);
         currentImportLabel.setText(currentImportedFile);
         remainingFilesLabel.setText(remainingFiles);
-    }
-
-    /**
-     * Set the status of whether a gps is connected or not.
-     *
-     * @param status the new status.
-     */
-    public void setStatus(String status) {
-        statusLabel.setText("GPS: " + status);
     }
 
     /**
