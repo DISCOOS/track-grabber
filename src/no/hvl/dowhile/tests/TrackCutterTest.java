51c62e113f1bf312d8b72dbb804ec26faa9f3599
VidarUre <vidarure@gmail.com>
2017-04-21 14:11:48 +0200

    Added a unit test for the TrackCutter. Testing on cutting a file based on start date.

---
+++ src/no/hvl/dowhile/tests/TrackCutterTest.java
@@ -0,0 +1,54 @@
+package no.hvl.dowhile.tests;
+
+import no.hvl.dowhile.core.OperationManager;
+import no.hvl.dowhile.core.TrackCutter;
+import no.hvl.dowhile.core.TrackInfo;
+import no.hvl.dowhile.utility.TrackTools;
+import org.alternativevision.gpx.beans.GPX;
+import org.alternativevision.gpx.beans.Track;
+import org.alternativevision.gpx.beans.Waypoint;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Date;
+
+import static junit.framework.TestCase.assertFalse;
+import static junit.framework.TestCase.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+public class TrackCutterTest {
+
+    OperationManager opManager;
+    TrackCutter cutter;
+    GPX gpxFile;
+    Track track;
+    ArrayList<Waypoint> trackPoints;
+
+    @Before
+    public void before() {
+        gpxFile = TrackTools.parseFileAsGPX(new File("src/testFile.gpx"));
+        track = TrackTools.getTrackFromGPXFile(gpxFile);
+        trackPoints = track.getTrackPoints();
+        opManager = new OperationManager();
+        cutter = new TrackCutter(opManager);
+        cutter.setTrackInfo(new TrackInfo("", 0, 0, "", 0));
+        cutter.setTrackFile(gpxFile);
+    }
+
+    @Test
+    public void trackIsFilteredOnStartTime() {
+
+        Waypoint firstTrackpoint = trackPoints.get(0);
+        Waypoint lastTrackpoint = trackPoints.get(trackPoints.size()-1);
+
+        long firstPointStart = firstTrackpoint.getTime().getTime();
+        long lastPointStart = lastTrackpoint.getTime().getTime();
+        long middleTime = (firstPointStart + lastPointStart) / 2;
+
+        GPX cutFile = cutter.filterOnTimeStarted(new Date(middleTime));
+
+        assertFalse(track.getTrackPoints().contains(firstTrackpoint));
+    }
+}
