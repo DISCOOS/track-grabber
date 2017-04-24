9ab5cf0917d245dc427ca8dab39a082786ed1f45
Jarand95 <jarand95@gmail.com>
2017-04-24 11:20:24 +0200

    Fixed an issue causing files to import before operation is setup.

--- src/no/hvl/dowhile/Main.java
+++ src/no/hvl/dowhile/Main.java
@@ -14,6 +14,5 @@ public class Main {
      */
     public static void main(String[] args) {
         OperationManager operationManager = new OperationManager();
-        operationManager.start();
     }
 }
