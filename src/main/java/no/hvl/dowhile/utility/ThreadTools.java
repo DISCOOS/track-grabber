387f5984e1dd261e10915e15c43f7e6766de2a2d
Jarand95 <jarand95@gmail.com>
2017-05-03 10:59:17 +0200

    Missing Javadoc.

--- src/main/java/no/hvl/dowhile/utility/ThreadTools.java
+++ src/main/java/no/hvl/dowhile/utility/ThreadTools.java
@@ -1,6 +1,14 @@
 package no.hvl.dowhile.utility;
 
+/**
+ * Utility methods for making it easier to work with threads.
+ */
 public class ThreadTools {
+    /**
+     * Make the thread sleep for the specified amount of seconds.
+     *
+     * @param seconds amount of seconds to sleep.
+     */
     public static void sleep(int seconds) {
         try {
             Thread.sleep(seconds * 1000);
